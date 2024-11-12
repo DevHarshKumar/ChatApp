2fa12341c18401edb9d307ffc666e540afe5ff18
Harsh Kumar <harsh.kumar@suffescom.com>
2024-11-12 16:33:28 +0530

    second

--- auth/src/Components/Navbar.js
+++ auth/src/Components/Navbar.js
@@ -25,7 +25,7 @@ return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 <NavLink
-    to="/home"
+    to="/"
     className="flex items-center space-x-3 rtl:space-x-reverse"
 >
     <img
